2dc37c76bd5afab2b237d5ad76089bb0a9a238a8
Gustavo Flores <gustavofc65@gmail.com>
2019-12-17 17:32:25 -0600

    [ADD] initial commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Sale Order - Brand",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "gflores",
+    'website': "http://www.gruporequiez.com",
+    'category': 'Sale',
+    'version': '12.0.0.0.1',
+    'depends': ['sale', 'product_brand', 'account'],
+    'data': [
+        'view/sale_view.xml',
+        'view/account_invoice_view.xml',
+    ],
+    'demo': [
+    ],
+}
